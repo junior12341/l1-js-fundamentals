5c67f660b93b0429e81532030f06e542b7497f36
Hubert Jr <hubertjunior254@icloud.com>
2022-11-21 12:25:13 +0300

    classwork done

--- index.js
+++ index.js
@@ -5,40 +5,48 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const pi = 3.14
 
 //TODO: ADD redeclarable variable
 
 
 //TODO: ADD re-assignable but not redeclarable variable
-
-
+var score = 100
+var score = 48
+score = 99 
+printValue (score)
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
+let age = 17
+printValue(age) // 17
 
 
 //TODO: Create a redeclarable variable with a boolean value
-
+var t= true 
 
 //TODO: Create a re-assignable variable with a string value
-
+let isEligibletoday ='mine'
 
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
-
+const message = 'I am happy'
+const time = '12:00 PM'
+const full = message + time
+printValue(full)
 
 //TODO: Interpolate string (`${}`)
-
+const other = `Hello ${message} and the time is ${time}`
+printValue(other)
 
 //TODO: Convert to uppercase
-
-
+const otherVariable = other.toUpperCase()
+printValue(otherVariable)
 //TODO: Convert to lowercase
-
+printValue(other.toLowerCase())
 
 //TODO: Index a specific character
-
+printValue("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(9))
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
